6574f127c82c49478464325db0972eefdf959bef
76200 <bartlomiej.marek.czyz@gmail.com>
2014-06-04 22:39:11 +0200

    Updated install script. It now creates `config` dir and puts there `google.key` file

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 ./bin/composer.phar dump-autoload --optimize
+mkdir -p config
+
+if [ ! -f ./config/google.key ]; then
+    echo "paste-your-api-key" > ./config/google.key
+    echo "Warning! Paste your GooglePlacesAPI key to './config/google.key'"
+fi
