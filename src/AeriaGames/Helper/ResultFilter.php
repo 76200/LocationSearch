3454943b28a2b774a9ffa6d625285b06ffa6526d
76200 <bartlomiej.marek.czyz@gmail.com>
2014-06-04 22:34:16 +0200

    Created ResultFilter

---
+++ src/AeriaGames/Helper/ResultFilter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AeriaGames\Helper;
+
+/**
+ * Class ResultFilter
+ * Helper to get filtered data containing only names and locations
+ */
+final class ResultFilter
+{
+    /**
+     * @var array
+     */
+    private $results;
+
+    /**
+     * @param array $results
+     */
+    function __construct($results)
+    {
+        $this->results = $results;
+    }
+
+    /**
+     * Filters result. Returns only names and addresses
+     *
+     * @return array
+     */
+    public function filter()
+    {
+        $arr = [];
+        foreach ($this->results as $result) {
+            if ($result['formatted_address'] && $result['name']) {
+                $arr[] = [
+                    'name' => $result['name'],
+                    'formatted_address' => $result['formatted_address']
+                ];
+            }
+        }
+
+        return $arr;
+    }
+
+}
